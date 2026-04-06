8b9dce0ff19468d2cab358577d4064ebec4d97e3
andredeveloper1995 <andreveloper1995@gmail.com>
2026-04-06 19:30:11 +0100

    Beta 0.0.1

---
+++ resources/[carros]/flickkkrise/fxmanifest.lua
@@ -0,0 +1,50 @@
+fx_version 'adamant'
+game 'gta5'
+author 'Flickkk'
+lua54 'on'
+
+files {
+	"data/**/**.meta",
+	"audioconfig/*",
+	"sfx/**/*"
+
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/s55b30_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/s55b30_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/s55b30_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_s55b30"
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/urusv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/urusv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_urusv8'
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/kc28sr180_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/kc28sr180_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/kc28sr180_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_kc28sr180"
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg51uruscapri_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg51uruscapri_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg51uruscapri_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg51uruscapri"
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/kc24r33gts_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/kc24r33gts_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/kc24r33gts_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_kc24r33gts"
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg81hcredeye_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg81hcredeye_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg81hcredeye_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg81hcredeye"
