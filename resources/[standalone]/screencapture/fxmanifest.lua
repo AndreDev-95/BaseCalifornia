7f4885fd42f8082a1b98e87de92639b169a55ea1
andredeveloper1995 <andreveloper1995@gmail.com>
2026-04-07 13:24:45 +0100

    Beta 0.0.1

---
+++ resources/[standalone]/screencapture/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+
+version '0.11.0'
+
+game "gta5"
+
+node_version '22'
+
+client_script "game/dist/client.js"
+server_script "game/dist/server.js"
+
+ui_page "game/nui/dist/index.html"
+files {
+    "game/nui/dist/index.html",
+    "game/nui/dist/**/*",
+}
+
+provide 'screenshot-basic'
